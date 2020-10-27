584e074613f39442db358ebbee4b4c66bf733781
HAII-LAB <seongminha0910@gmail.com>
2020-10-28 03:23:00 +0900

    made code clear

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
-const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
   const date = new Date();
@@ -9,7 +9,6 @@ function getTime() {
   clockTitle.innerText = 
     `${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10?`0${seconds}`:seconds}`;
 }
-
 function init() {
   getTime();
   setInterval(getTime, 1000);
--- greeting.js
+++ greeting.js
@@ -1,21 +1,19 @@
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
 
 function saveName(text){ // 로컬 저장소에 유저 이름 저장
     localStorage.setItem(USER_LS, text);
 }  
-
 function handleSubmit(event){ // Submit Event 에서 수행할 작업
     event.preventDefault(); // 기본값이 새로고침 -> 무반응
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
 }
-
 function askForName(){ // 유저 이름 Submit 처리
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit",handleSubmit);
@@ -25,7 +23,6 @@ function paintGreeting(text) { // 유저 이름 화면 출력
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
-  
 function loadName() { // 유저 이름의 저장 상태에 따라 함수 호출
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
@@ -37,4 +34,5 @@ function loadName() { // 유저 이름의 저장 상태에 따라 함수 호출
 function init(){
     loadName();    
 }
+
 init();
