60c4edd16fed9135e286ac34a5873c63bcadabcf
HAII-LAB <seongminha0910@gmail.com>
2020-10-30 03:58:53 +0900

    added delete todos

--- todo.js
+++ todo.js
@@ -4,15 +4,26 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li); // HTML에서 삭제
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id != parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.inner = "❌";
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click",deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text;
