fbef1214428d25afef5c8c5c7331ab105648934b
HAII-LAB <seongminha0910@gmail.com>
2020-11-06 01:41:35 +0900

    fixed to-do list text error

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 const toDoForm = document.querySelector(".js-toDoForm");
-const toDoInput = form.querySelector("input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
@@ -22,11 +22,14 @@ function saveToDos(){
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
-    delBtn.addEventListener("click",deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click",deleteToDo);
+    
     span.innerText = text;
+    
     li.appendChild(span);
     li.appendChild(delBtn);
     li.id = newId;
@@ -36,17 +39,17 @@ function paintToDo(text){
         id: newId
     };
     toDos.push(toDoObj);
-    savetToDos();
+    saveToDos();
 }
 function handleSubmit(event){
-    preventDefault();
+    event.preventDefault();
     const currentValue=toDoInput.value;
     paintToDo(currentValue);
     toDoInput.value="";
 }
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
-    if(loadedToDos != null){
+    if(loadedToDos !== null){
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
